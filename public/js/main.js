c776e06fad05b17a64fd20de9321a230e534e4ee
OpenShift guest <anamariauribe1998@gmail.com>
2014-04-01 20:58:37 -0400

    Autenticacion

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,35 @@
-$(function () {
+$(function() {
 
-  var signIn = $("#signin");
+    var success = $(".alert-success");
+    var error = $(".alert-danger");
+    success.hide();
+    error.hide();
 
-  signIn.on('click', function (e) {
-    e.preventDefault();
 
-    var username = $("#email").val();
-    var password = $("#password").val();
 
-    console.log(username, password);
-  });
 
- });
+    var signIn = $("#signin");
+
+    signIn.on('click', function(e) {
+        e.preventDefault();
+
+        var username = $("#email").val();
+        var password = $("#password").val();
+
+        //Verificar el usuario y la contrasena
+
+
+        if (username == "anamaria" && password == "anamaria123") {
+            error.hide()
+            success.show()
+        }
+        else {
+            success.hide()
+            error.show()
+
+        }
+
+
+
+    });
+});
